28f399924823e38c25e067630a2de892c778b14c
middlefgr <2giwon@gmail.com>
2020-11-15 21:22:37 +0900

    [생성] - HILT 디펜던시 추가

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -8,6 +8,7 @@ object ProjectConfig {
     const val KOTLIN_GRADLE_PLUGIN =
         "org.jetbrains.kotlin:kotlin-gradle-plugin:${Version.KOTLIN_VER}"
     const val ANDROID_JUNIT5 = "de.mannodermaus.gradle.plugins:android-junit5:1.6.2.0"
+    const val HILT_ANDROID_GRADLE_PLUGIN = "com.google.dagger:hilt-android-gradle-plugin:2.28-alpha"
 }
 
 object AndroidConfig {
@@ -20,10 +21,19 @@ object AndroidConfig {
 }
 
 object Dependencies {
+    private const val HILT_JETPACK_VER = "1.0.0-alpha01"
+    private const val HILT_VER = "2.28-alpha"
+
     const val KOTLIN_STDLIB = "org.jetbrains.kotlin:kotlin-stdlib:${Version.KOTLIN_VER}"
     const val CORE_KTX = "androidx.core:core-ktx:1.3.0"
     const val MATERIAL = "com.google.android.material:material:1.1.0"
     const val CONSTRAINT_LAYOUT = "androidx.constraintlayout:constraintlayout:2.0.1"
+
+    const val HILT_ANDROID = "com.google.dagger:hilt-android:$HILT_VER"
+    const val HILT_ANDROID_COMPILER = "com.google.dagger:hilt-android-compiler:$HILT_VER"
+
+    const val DAGGER_HILT_LIFECYCLE_VM = "androidx.hilt:hilt-lifecycle-viewmodel:$HILT_JETPACK_VER"
+    const val HILT_COMPILER = "androidx.hilt:hilt-compiler:$HILT_JETPACK_VER"
 
     const val ROOM_RUNTIME = "androidx.room:room-runtime:${Version.ROOM_VER}"
     const val ROOM_COMPILER = "androidx.room:room-compiler:${Version.ROOM_VER}"
